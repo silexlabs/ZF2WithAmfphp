8bbe9d8e29437dd3cffda7dd4d465e3fb524ac7e
BenjaminG1983 <forum2@dfbl.org>
2014-02-07 19:06:56 +0100

    FIXED: wrong amf class name

---
+++ module/Service/src/Service/Model/Amf/Test.php
@@ -0,0 +1,14 @@
+<?php
+  class Test
+  {
+    /**
+     * Get Altersstufe list
+     *
+     * @return array
+     */
+    public function getList()
+    {
+      return 'This is a test.';
+    }
+  }
+?>
